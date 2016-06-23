e6eb1f1b42945c9df0d4347dd6337b8b9e3b73c8
Dan Dietz <danddietz@gmail.com>
2016-06-22 22:43:51 -0400

    WIP: more general updates and design integration.

--- app/scripts/components/login.jsx
+++ app/scripts/components/login.jsx
@@ -78,38 +78,86 @@ var LoginComponent = React.createClass({
     },
     render: function(){
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12 text-center">
-                        <h1>Start Your Engine!</h1>
+            <section className="body-mobilewrapper container-LandingPageBody">
+
+                <div className="field-Text">
+                    <div id="mc_embed_signup">
+                        <div className="image-TruckPhotoText">Login</div>
+
+                        <form target="_blank" novalidate>
+                            <input type="email" name="username" className="email form-control" placeholder="username" required="required" />
+
+                            <input type="email" name="email" className="password form-control" placeholder="password" required="required" />
+
+                            <div className="button-NavContainerOuter">
+                                <div className="button-NavContainerInner">
+                                    <input type="submit" className="" value="Login" />
+                                </div>
+                            </div>
+                        </form>
+
+                        <div className="image-TruckPhotoText">
+                        or Register
+                        </div>
+            
+            
+                        <form  target="_blank" novalidate>
+                            <input type="email" name="username" className="username form-control" placeholder="username" required="required" />
+
+                            <input type="password" name="password" className="username form-control" placeholder="password" required="required" />
+
+                            <input type="password" name="password2" className="username form-control" placeholder="reenter password" required="required" />
+
+                            <input type="text" name="truck" className="username form-control" placeholder="food truck name" required="required" />
+
+                            <input type="text" name="phone" className="username form-control" placeholder="phone number" required="required" />
+
+                            <input type="email" name="email" className="username form-control" placeholder="email address" required="required" />
+
+                            <div className="button-NavContainerOuter">
+                                <input type="submit" value="register" className="button-NavContainerInner button-NavText"/>
+                            </div>
+                        </form>
+            
                     </div>
                 </div>
-
-                <div className="row">
-                    <div className="col-sm-12">
-                        <form id="login" onSubmit={this.handleSignIn}>
-                            <Input name="username" type="text" placeholder="username" valueLink={this.linkState('username')}/>
-                            <Input name="password" type="password" placeholder="Password Please" valueLink={this.linkState('password')}/>
-
-                            <ButtonInput className="btn btn-primary" type="submit" value="Login" />
-                        </form>
-                    </div>
-
-                    <div className="col-sm-12">
-                        <form id="signup" onSubmit={this.handleSignup}>
-                            <Input name="username" type="text" placeholder="username" valueLink={this.linkState('usernameUp')}/>
-                            <Input name="password" type="password" placeholder="password" valueLink={this.linkState('passwordUp')}/>
-                            <Input name="password2" type="password" placeholder="reenter password" valueLink={this.linkState('password2Up')}/>
-                            <Input name="truck" type="text" placeholder="food truck name" valueLink={this.linkState('truckName')}/>
-                            <Input name="phone" type="text" placeholder="phone number" valueLink={this.linkState('phone')}/>
-                            <Input name="email" type="email" placeholder="email" valueLink={this.linkState('email')}/>
-
-                            <ButtonInput className="btn btn-info" type="submit" value="Register" />
-                        </form>
-                    </div>
+            
+            </section>
+        );
+            
+        {/*
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12 text-center">
+                    <h1>Start Your Engine!</h1>
                 </div>
             </div>
-        );
+
+            <div className="row">
+                <div className="col-sm-12">
+                    <form id="login" onSubmit={this.handleSignIn}>
+                        <Input name="username" type="text" placeholder="username" valueLink={this.linkState('username')}/>
+                        <Input name="password" type="password" placeholder="Password Please" valueLink={this.linkState('password')}/>
+
+                        <ButtonInput className="btn btn-primary" type="submit" value="Login" />
+                    </form>
+                </div>
+
+                <div className="col-sm-12">
+                    <form id="signup" onSubmit={this.handleSignup}>
+                        <Input name="username" type="text" placeholder="username" valueLink={this.linkState('usernameUp')}/>
+                        <Input name="password" type="password" placeholder="password" valueLink={this.linkState('passwordUp')}/>
+                        <Input name="password2" type="password" placeholder="reenter password" valueLink={this.linkState('password2Up')}/>
+                        <Input name="truck" type="text" placeholder="food truck name" valueLink={this.linkState('truckName')}/>
+                        <Input name="phone" type="text" placeholder="phone number" valueLink={this.linkState('phone')}/>
+                        <Input name="email" type="email" placeholder="email" valueLink={this.linkState('email')}/>
+
+                        <ButtonInput className="btn btn-info" type="submit" value="Register" />
+                    </form>
+                </div>
+            </div>
+        </div>
+        */}
     }
 });
 
